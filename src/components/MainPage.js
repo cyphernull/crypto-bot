7881f56be1e16212fd1c57f43d93645ac03b48d0
Eric Choo <eric.choo1997@yahoo.com>
2018-03-20 17:39:54 +0800

    ini

---
+++ src/components/MainPage.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+class MainPage extends Component {
+  render() {
+    return <div>hi</div>
+  }
+}
+export default MainPage
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MainPage from './components/MainPage'
+import './styles/index.css'
+ReactDOM.render(<MainPage />, document.getElementById('root'))
